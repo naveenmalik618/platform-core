a43cd4dfdcba1fbd3fc07eb1825c22e3044221d8
nmalik239 <nmalik239@gmail.com>
2025-03-05 21:42:34 +0530

    initial commit

---
+++ src/test/java/com/malik/platform_core/PlatformCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malik.platform_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
